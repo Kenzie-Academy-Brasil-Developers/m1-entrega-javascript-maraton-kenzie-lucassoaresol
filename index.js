61492ec8c6cb520e702858d8b63250e487113ea8
Lucas <lucassoares2007@hotmail.com>
2022-07-06 17:54:34 -0300

    correções

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let competidores = ['Rafael','Manoel','Daniel']
 function positions(array){
     let posicaoDaniel = array.indexOf('Daniel')
     if(posicaoDaniel!=0){
-        let provisorio = array[0]
-        array[0] = 'Daniel'
+        let provisorio = array[posicaoDaniel-1]
+        array[posicaoDaniel-1] = 'Daniel'
         array[posicaoDaniel] = provisorio
     }
     else{
